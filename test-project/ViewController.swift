f3dd29a184f80d7d521e32a0eb18cdc03e9e480b
Andrey Kats <andrey@gramercytech.com>
2018-10-31 13:50:58 -0400

    First commit

---
+++ test-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-project
+//
+//  Created by Andrey on 10/31/18.
+//  Copyright © 2018 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
